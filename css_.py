9da69f438c886927d45521d83b7522abd62aebdc
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2022-01-09 12:59:55 -0500

    moved css stuff to its own file

---
+++ css_.py
@@ -0,0 +1,17 @@
+css = '''
+.bk-root .bk-tabs-header {
+  background: #000000;
+  border-radius: 5px;
+  border: 1px black solid;
+  font-size: x-large;
+  background-color: black;
+}
+
+p {
+  font-size: 16px;
+}
+
+li {
+  font-size: 16px;
+}
+'''
